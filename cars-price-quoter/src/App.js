9dc5edfd2c3fe721facef92c059717e8d961b6ab
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-08-29 12:52:15 -0300

    87) Creando los Primeros Styled Components

--- cars-price-quoter/src/App.js
+++ cars-price-quoter/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// Imports
+
+
+import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// Components
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const ContainerForm = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <Header title="Cotizador de Seguros"/>
+   <Container>
+       <Header title="Cotizador de Seguros"/>
+       <ContainerForm>
+        
+       </ContainerForm>
+   </Container>
   );
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// Exports
+
 
 export default App;
--- cars-price-quoter/src/components/Header.jsx
+++ cars-price-quoter/src/components/Header.jsx
@@ -1,12 +1,43 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// Imports
+
+
 import React from 'react'
+import styled from '@emotion/styled'
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// Components
+
+
+const HeaderContainer = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
+
 
 
 const Header = ({title}) => {
     return ( 
-        <header>
-            <h1>{title}</h1>
-        </header>
+        <HeaderContainer>
+            <TextHeader>{title}</TextHeader>
+        </HeaderContainer>
      );
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// Exports
+
  
 export default Header;
